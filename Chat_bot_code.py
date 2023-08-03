be4dd31d0ae5377b86dd5347c416c46fb5b68211
Awaassa <140975428+Awaassa@users.noreply.github.com>
2023-08-04 00:02:55 +0530

    Update Chat_bot_code.py

--- Chat_bot_code.py
+++ Chat_bot_code.py
@@ -47,7 +47,9 @@ def chatbot():
     while True:
         user_input: str = input("You: ")
 
-        if user_input.lower() == 'bye':
+        if user_input.lower() == 'exit':
+            break
+        elif user_input.lower() == 'bye':
             break
 
 
@@ -57,14 +59,7 @@ def chatbot():
             answer: str = get_answer_for_question(best_match, knowledge_base)
             print(f"Bot: {answer}")
         else:
-            print("Bot: I don't know the answer. Can you teach me?")
-            new_answer: str = input("Type the answer or 'skip' to skip: ")
-
-            if new_answer.lower() != 'skip':
-                knowledge_base["question"].append({"question": user_input, "answer": new_answer})
-                save_knowledge_base('knowledge_base.json', knowledge_base)
-                print("Bot: Thank you! I've learned something new.")
-
+            print("Bot: I am not able to find answer to your question but I can refer you to the school website 'http://www.agdav.edu.in'? ")
 
 if __name__ == "__main__":
     chatbot()
